12809a428875863c1f45e6440cf2ed91bdeb2aaf
Alicia Muniz <alicia.muniz@gestaopublica.etc.br>
2025-02-21 18:21:39 -0300

    como C interpreta booleano e correção do texto em referencias

---
+++ booleano.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main () {
+    //0 = false, 1 = true
+    int x = 10;
+    printf("teste: %d", !(x == 10));
+
+    return 0;
+}
--- referencias.c
+++ referencias.c
@@ -7,7 +7,7 @@ int main () {
     printf("endereco de a = %p\n",&a);
     printf("caracter equivalente de a: %c\n", a); //tabela ascii
     //printf("string de a: %s\n", a); //erro de acesso à memoria no endereço
-    printf("string da referencia de a: %s\n", &a); 
+    printf("string da referencia de a: %s\n", &a); //operador referencia
 
     return 0;
 }
